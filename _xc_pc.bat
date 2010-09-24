26948f1de77b11f27b38cb214478f9ac8bac5b53
galippi <liptakgabor@freemail.hu>
2010-09-24 12:24:41 +0200

    Environment change: start with other name

--- _xc_pc.bat
+++ _xc_pc.bat
@@ -1,2 +1,7 @@
-@set HOME=D:\users\liptak
-output\PC\bin\XCSoar.exe
+::@echo off
+set HOME=D:\users\liptak
+del D:\users\liptak\.xcsoar\xcsoar-startup.log
+if exist c:\temp\conf.exe del c:\temp\conf.exe
+copy output\PC\bin\XCSoar.exe c:\temp\conf.exe
+c:\temp\conf.exe
+if exist D:\users\liptak\.xcsoar\xcsoar-startup.log start D:\users\liptak\.xcsoar\xcsoar-startup.log
